792fffd8f0f737f7bcffe47b1d4802830edf9075
mahbub <mahbub.shuvo10@gmail.com>
2025-11-18 22:19:35 +0100

    product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
+@Module({
+  controllers: [ProductController],
+  providers: [ProductService],
+  imports: [PrismaModule, AuthModule],
+})
 export class ProductModule {}
